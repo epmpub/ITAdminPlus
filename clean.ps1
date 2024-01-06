f554bb53eb6e84683f046923ba6569f264a41b6a
epmpub <you@example.com>
2024-01-06 14:51:03 +0800

    update build clean fileWatcher script

---
+++ clean.ps1
@@ -0,0 +1,6 @@
+Remove-Item .\forWin.exe -ErrorAction SilentlyContinue
+Remove-Item .\forArm  -ErrorAction SilentlyContinue
+Remove-Item .\forLinux  -ErrorAction SilentlyContinue
+Remove-Item .\forMac  -ErrorAction SilentlyContinue
+Remove-Item .\forMac  -ErrorAction SilentlyContinue
+Remove-Item .\md5.json  -ErrorAction SilentlyContinue
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 // author: andy.hu
 // email:andy.hu.sheng@gmail.com
 // date: 2024/1/5 16:11
+// version: v1.0
+// change log: 1.UPDATE AAAA BBBB MD5 VALUE
 
 package main
 
